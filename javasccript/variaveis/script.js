25b8434681b8ba37fcaa728f19d066a75887b9f2
Robson Aguiar <robson.aguiarart@hotmail.com>
2026-02-16 19:02:16 -0300

    Mais sobre variáveis e diferenças entre var, const e let

--- javasccript/variaveis/script.js
+++ javasccript/variaveis/script.js
@@ -15,3 +15,21 @@ console.log(sobrenome, cidade);
 // Sem valor
 var semDefinir;
 console.log(semDefinir); //undefined
+/*console.log(aindaNaoDefini);*/
+
+// Hoisting;
+
+console.log(profissao); //Ainda não criei a variável
+var profissao = "Técnico SericeDesk";
+console.log(profissao);
+
+// Mudar o vaor da variável
+
+var time = "vasco";
+var time = "flamengo";
+console.log(time);
+
+// let time1 = "vasco"; // não consegue criar novamente usando let
+// let time1 = "flamengo"; // não consegue criar novamente usando let
+// const time2 = "vasco"; // não pode modificar o valor da variável
+// time2 = "flamengo";// não pode modificar o valor da variável
