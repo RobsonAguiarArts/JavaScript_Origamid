78547fb59e8487c184288ebf9ccbca1b78db3c6c
Robson Aguiar <robson.aguiarart@hotmail.com>
2026-02-16 18:52:32 -0300

    Aprendendo sobre variáveis

---
+++ javasccript/variaveis/script.js
@@ -0,0 +1,17 @@
+// variáveis
+var nome = "Robson";
+var idade = 23;
+possuiFaculdade = true;
+console.log(nome, idade, possuiFaculdade);
+// Evitam repetições Sintaxe
+var preco = 25;
+var totalComprado = 5;
+var precoTotal = preco * totalComprado;
+console.log(precoTotal);
+// virgula
+var sobrenome = "Aguiar",
+  cidade = "São Paulo";
+console.log(sobrenome, cidade);
+// Sem valor
+var semDefinir;
+console.log(semDefinir); //undefined
