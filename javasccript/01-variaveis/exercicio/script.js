31d011c2e4025789d5fb17b894ede09ae16aae61
Robson Aguiar <robson.aguiarart@hotmail.com>
2026-03-06 10:21:50 -0300

    Aprendendo sobre tipos de dados e variáveis

---
+++ javasccript/01-variaveis/exercicio/script.js
@@ -0,0 +1,19 @@
+// Dica rápira, frases (string) devem ser colocdas entre aspas (simpres ou duplas) e números não levam aspas.
+
+// Declarar uma variável com seu nome
+const nome = "Robson";
+
+// Declarar uma variável com a sua idade
+const idade = 38;
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Pizza";
+
+// Declarar 5 variáveris diferentes sem valores
+
+var carro, moto, salario, cor, cidade;
+
+console.log(nome, idade, comidaFavorita);
---
+++ javasccript/02-Tipos de dados/script.js
@@ -0,0 +1,37 @@
+// Todos são primitivos exceto os objetos.
+
+var nome = "Robson"; //string
+console.log(nome, typeof nome);
+var sobrenome = "Aguiar";
+var nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase = "Romário ez " + gols + " gols";
+console.log(typeof frase);
+
+var ano = "2018";
+var mes = 8;
+console.log(ano + mes);
+var melhor = "teste";
+var frase1 = "Esse é o " + melhor + " jogo";
+console.log(frase1);
+
+// -----------------------------------
+var idade = 38; //Number
+console.log(idade, typeof idade);
+// -----------------------------------
+var possuiFaculdade = true; // Boolean
+console.log(possuiFaculdade, typeof possuiFaculdade);
+// -----------------------------------
+var time; // Undefined
+console.log(time, typeof time);
+// -----------------------------------
+var comida = null; // Null
+console.log(comida, typeof comida);
+// -----------------------------------
+var simbolo = Symbol; // Symbol
+console.log(simbolo, typeof simbolo);
+// -----------------------------------
+var novoObjeto = {}; //Object
+console.log(novoObjeto, typeof novoObjeto);
